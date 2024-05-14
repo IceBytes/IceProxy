6ee65b527f0b0c6a43416ebc959813fee91a0207
JustIce <justice@JustIce.JustIce>
2024-05-14 07:53:36 -0500

    Add download proxies

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from functions.db import ProxyDatabase
 from math import ceil
+import json
 
 app = Flask(__name__)
 db = ProxyDatabase()
 
-@app.route('/', methods=['GET'])
+PER_PAGE = 100
+
+@app.route('/')
 def index():
-    page = request.args.get('page', default=1, type=int)
-    per_page = 100
-    total_proxies = db.len_proxies()
-    total_pages = ceil(total_proxies / per_page)
-    start_index = (page - 1) * per_page
-    end_index = start_index + per_page
+    page = int(request.args.get('page', 1))
+    start_index = (page - 1) * PER_PAGE
+    end_index = start_index + PER_PAGE
     proxies = db.fetch_all_proxies()[start_index:end_index]
-
+    total_proxies = db.len_proxies()
+    total_pages = ceil(total_proxies / PER_PAGE)
     has_next_page = page < total_pages
-
     return render_template("index.html", proxies=proxies, page=page, has_next_page=has_next_page)
 
-@app.route('/docs', methods=['GET'])
+@app.route('/download')
+def download_proxies():
+    format = request.args.get('format')
+    proxies = db.fetch_all_proxies()
+    if format == 'text':
+        proxies_text = "\n".join([f'{proxy["ip"]}:{proxy["port"]}' for proxy in proxies])
+        return Response(proxies_text, mimetype="text/plain", headers={"Content-Disposition": "attachment;filename=proxies.txt"})
+    elif format == 'json':
+        proxy_list = [{"ip": proxy["ip"], "port": proxy["port"], "protocol": proxy["protocol"], "ssl": proxy["ssl"],
+                       "country": proxy["country"], "status": proxy["status"], "speed": proxy["speed"]} for proxy in proxies]
+        return Response(json.dumps(proxy_list), mimetype='application/json', headers={"Content-Disposition": "attachment; filename=proxies.json"})
+    return "Invalid format", 400
+
+@app.route('/docs')
 def docs():
     return render_template("docs.html")
 
